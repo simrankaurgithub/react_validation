c7765a4f316f0c8611c03454d00d7995f40b0d3b
simrankaurgithub <simr35478@gmail.com>
2022-11-28 18:24:51 +0530

    simran

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,32 +1,162 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function validation() {
+
+export default function Register() {
+    const [unameErr, setunameErr] = useState('')
+    const [fnameErr, setfnameErr] = useState('')
+    const [lnameErr, setlnameErr] = useState('')
+    const [emailErr, setemailErr] = useState('')
+    const [passErr1, setpassErr1] = useState('')
+    const [passErr2, setpassErr2] = useState('')
+    const keyname = () => {
+        let uname = document.getElementById("id_username").value;
+        console.log(uname)
+        let re = /^[A-Za-z0-9/_@/./-]*$/
+        if (!(re.test(uname))) {
+            setunameErr("Username can't contain special character except (@,.,+,-,_ ) ");
+        }
+        else if (uname.length < 4) {
+            setunameErr("Username should have atleast 4 characters");
+        }
+        else if (uname !== "") {
+            setunameErr("");
+        }
+        if (uname.length > 100) {
+            setunameErr("Username can contain maximum 100 characters");
+        }
+    }
+    const blurname = () => {
+        let uname = document.getElementById("id_username").value
+        if (uname === "") {
+            setunameErr("Username is required");
+        }
+    }
+    const keyfname = () => {
+        let fname = document.getElementById("id_firstname").value;
+        let re = /^[A-Za-z]*$/
+        if (!(re.test(fname))) {
+            setfnameErr("First name can contian alphabet only");
+        }
+        else if (fname !== "") {
+            setfnameErr("");
+        }
+        if (fname.length > 100) {
+            setfnameErr("First name can contain maximum 100 characters");
+        }
+    }
+    const blurfname = () => {
+        let fname = document.getElementById("id_firstname").value
+        if (fname === "") {
+            setfnameErr("First Name is required");
+        }
+    }
+    const keylname = () => {
+        let lname = document.getElementById("id_lastname").value;
+        let re = /^[A-Za-z]*$/
+        if (!(re.test(lname))) {
+            setlnameErr("Last Name can contian alphabet only");
+        }
+        else if (lname !== "") {
+            setlnameErr("");
+        }
+        if (lname.length > 100) {
+            setlnameErr("Last Name can contain maximum 100 characters");
+        }
+    }
+    const keyemail = () => {
+        let uemail = document.getElementById("id_email").value
+        let re = /^\w+([\.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+        if (uemail === "") {
+            setemailErr("");
+        }
+        else if (!(re.test(uemail))) {
+            setemailErr("Email be like 'example@abc.efg'");
+        }
+        else if (re.test(uemail)) {
+            setemailErr("");
+        }
+    }
+    const bluremail = () => {
+        let uemail = document.getElementById("id_email").value;
+        if (uemail === "") {
+            setemailErr("Email is required") ;
+        }
+    }
+    const keypass1 = () => {
+        let upass = document.getElementById("id_password1").value;
+        if (upass !== "") {
+            setpassErr1("");
+        }
+        if (upass.length > 200) {
+            setpassErr1("Password can contain maximum 200 characters");
+        }
+        else if (upass.length < 200) {
+            setpassErr1("");
+        }
+    }
+    const blurpass1 = () => {
+        let upass = document.getElementById("id_password1").value;
+        if (upass === "") {
+            setpassErr1("Password is required");
+        }
+    }
+    const keypass2 = () => {
+        let upass = document.getElementById("id_password1").value;
+        let repass = document.getElementById("id_password2").value
+        if (repass !== "") {
+            setpassErr2("");
+        }
+        if (upass !== repass) {
+            setpassErr2("Password doesn't match");
+        }
+    }
+    const blurpass2 = () => {
+        let repass = document.getElementById("id_password2").value
+        if (repass === "") {
+            setpassErr2("Confirm password is required");
+        }
+    }
+    const inputpass1 = () => {
+        let upass = document.getElementById("id_password1").value;
+        if (upass !== "") {
+        }
+        if (upass.length > 200) {
+            setpassErr1("Password can contain maximum 200 characters");
+        }
+        else if (upass.length < 200) {
+            setpassErr1("");
+        }
+    }
     return (
-        <div class="image-container d-flex py-5 ">
+        <div class="image-container d-flex ">
             <form method="POST" novalidate
                 class="px-4 shadow-lg  mx-auto register-form bg-white col-xs-8 col-sm-8 col-md-5 col-lg-4 py-4">
                 <h2 class="text-center pb-3 text-primary">Register</h2>
                 <div class="my-3">
-                    <input type="text" class="form-control" id="id_username" placeholder='Enter Username' />
-                    <small id="name" class="text-danger"></small>
+                    <input type="text" class="form-control" id="id_username" placeholder='Enter Username' 
+                    onKeyUp={()=>{keyname()}} onBlur={()=>{blurname()}} />
+                    <small class="text-danger">{unameErr}</small>
                 </div>
                 <div class="row my-3">
                     <div class="col-6">
-                        <input type="text" class="form-control" id="id_firstname" placeholder='Enter First Name' />
-                        <small id="fname" class="text-danger"></small>
+                        <input type="text" class="form-control" id="id_firstname" placeholder='Enter First Name'
+                        onKeyUp={()=>{keyfname()}} onBlur={()=>{blurfname()}} />
+                        <small class="text-danger">{fnameErr}</small>
                     </div>
                     <div class="col-6">
-                        <input type="text" class="form-control" id="id_lastname" placeholder='Enter Last Name' />
-                        <small id="lname" class="text-danger"></small>
+                        <input type="text" class="form-control" id="id_lastname" placeholder='Enter Last Name'
+                        onKeyUp={()=>{keylname()}}  />
+                        <small class="text-danger">{lnameErr}</small>
                     </div>
                 </div>
                 <div class="my-3">
-                    <input type="email" class="form-control" id="id_email" placeholder='Enter Email' />
-                    <small id="email" class="text-danger"></small>
+                    <input type="email" class="form-control" id="id_email" placeholder='Enter Email' 
+                    onKeyUp={()=>{keyemail()}} onBlur={()=>{bluremail()}}/>
+                    <small class="text-danger">{emailErr}</small>
                 </div>
                 <div class="my-3">
-                    <input type="password" class="form-control" id="id_password1" placeholder='Enter Password' />
-                    <small id="pass1" class="text-danger"></small>
+                    <input type="password" class="form-control" id="id_password1" placeholder='Enter Password' onKeyUp={()=>{keypass1()}} onBlur={()=>{blurpass1()}} onInput={()=>{inputpass1()}} />
+                    <small id="pass1" class="text-danger">{passErr1}</small>
                 </div>
                 <div id="pswd_info">
                     <h6 class="px-3 fw-bold py-2">Password must contain : </h6>
@@ -45,11 +175,11 @@ export default function validation() {
                 </div>
                 <div id="hello">
                     <div class="my-3">
-                        <input type="password" class="form-control" id="id_password2" placeholder='Enter Confirm Password' />
-                        <small id="pass2" class="text-danger"></small>
+                        <input type="password" class="form-control" id="id_password2" placeholder='Enter Confirm Password' onKeyUp={()=>{keypass2()}} onBlur={()=>{blurpass2()}} />
+                        <small class="text-danger">{passErr2}</small>
                     </div>
                 </div>
-                <input type="submit" value="Register" class="my-2 w-100 btn btn-primary" />
+                <input type="submit" value="Register" class="my-3 w-100 btn btn-primary" />
             </form >
         </div >);
         //  $(document).ready(function () {
@@ -109,149 +239,7 @@ export default function validation() {
     
         // });
     
-    
-        const check_username = () => {
-            uname = document.getElementById("{{form.username.auto_id}}").value;
-            $.ajax({
-                url: "{% url 'check_user_exist' %}",
-                method: "GET",
-                data: { username: uname },
-                success: function (data) {
-                    console.log(data)
-                    if (data.status == 0) {
-                        document.getElementById("name1").innerText = "";
-                    }
-                    else {
-                        document.getElementById("name1").innerText = "Username already exists";
-                    }
-    
-                }
-            })
-        }
-    
-        const check_email = () => {
-            uemail = document.getElementById("{{form.email.auto_id}}").value;
-            $.ajax({
-                url: "{% url 'check_email_exist' %}",
-                method: "GET",
-                data: { email: uemail },
-                success: function (data) {
-                    console.log(data)
-                    if (data.status == 0) {
-                        document.getElementById("email1").innerText = "";
-                    }
-                    else {
-                        document.getElementById("email1").innerText = "Email already exists";
-                    }
-                }
-            })
-        }
-    
-        const keyname = () => {
-            let uname = document.getElementById("{{form.username.auto_id}}").value;
-            let re = /^[A-Za-z0-9/_@/./-]*$/
-            if (!(re.test(uname))) {
-                document.getElementById("name").innerText = "Username can't contain special character except (@,.,+,-,_ ) ";
-            }
-            else if (uname.length < 4) {
-                document.getElementById("name").innerText = "Username should have atleast 4 characters";
-            }
-            else if (uname !== "") {
-                document.getElementById("name").innerText = "";
-            }
-            if (uname.length > 100) {
-                document.getElementById("name").innerText = "Username can contain maximum 100 characters";
-            }
-        }
-        const blurname = () => {
-            let uname = document.getElementById("{{form.username.auto_id}}").value
-            if (uname === "") {
-                document.getElementById("name").innerText = "Username is required";
-            }
-        }
-        const keyfname = () => {
-            let uname = document.getElementById("{{form.first_name.auto_id}}").value;
-            let re = /^[A-Za-z]*$/
-            if (!(re.test(uname))) {
-                document.getElementById("fname").innerText = "First name can contian alphabet only";
-            }
-            else if (uname !== "") {
-                document.getElementById("fname").innerText = "";
-            }
-            if (uname.length > 100) {
-                document.getElementById("fname").innerText = "First name can contain maximum 100 characters";
-            }
-        }
-        const blurfname = () => {
-            let uname = document.getElementById("{{form.first_name.auto_id}}").value
-            if (uname === "") {
-                document.getElementById("fname").innerText = "First Name is required";
-            }
-        }
-        const keylname = () => {
-            let uname = document.getElementById("{{form.last_name.auto_id}}").value;
-            let re = /^[A-Za-z]*$/
-            if (!(re.test(uname))) {
-                document.getElementById("lname").innerText = "Last Name can contian alphabet only";
-            }
-            else if (uname !== "") {
-                document.getElementById("lname").innerText = "";
-            }
-            if (uname.length > 100) {
-                document.getElementById("lname").innerText = "Last Name can contain maximum 100 characters";
-            }
-        }
-        const keyemail = () => {
-            let uemail = document.getElementById("{{form.email.auto_id}}").value
-            let re = /^\w+([\.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
-            if (uemail === "") {
-                document.getElementById("email").innerText = "";
-            }
-            else if (!(re.test(uemail))) {
-                document.getElementById("email").innerText = "Email be like 'example@abc.efg'";
-            }
-            else if (re.test(uemail)) {
-                document.getElementById("email").innerText = "";
-            }
-        }
-        const bluremail = () => {
-            let uemail = document.getElementById("{{form.email.auto_id}}").value;
-            if (uemail === "") {
-                document.getElementById("email").innerText = "Email is required ";
-            }
-        }
-        const keypass1 = () => {
-            let upass = document.getElementById("{{form.password1.auto_id}}").value;
-            if (upass !== "") {
-                document.getElementById("pass1").innerText = ""
-            }
-            if (upass.length > 200) {
-                document.getElementById("pass1").innerText = "Password can contain maximum 200 characters";
-            }
-            else if (upass.length < 200) {
-                document.getElementById("pass1").innerText = "";
-            }
-        }
-        const blurpass1 = () => {
-            let upass = document.getElementById("{{form.password1.auto_id}}").value;
-            if (upass === "") {
-                document.getElementById("pass1").innerText = "Password is required"
-            }
-        }
-        const keypass2 = () => {
-            let upass = document.getElementById("{{form.password1.auto_id}}").value;
-            let repass = document.getElementById("{{form.password2.auto_id}}").value
-            if (repass !== "") {
-                document.getElementById("pass2").innerText = ""
-            }
-            if (upass !== repass) {
-                document.getElementById("pass2").innerText = "Password doesn't match"
-            }
-        }
-        const blurpass2 = () => {
-            let repass = document.getElementById("{{form.password2.auto_id}}").value
-            if (repass === "") {
-                document.getElementById("pass2").innerText = "Confirm password is required"
-            }
-        }
+        
+      
+      
 }
